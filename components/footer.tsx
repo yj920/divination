2b3a7a6d70f427dfa6c3d979b92949dfbd723fdc
yj920 <43157069+yj920@users.noreply.github.com>
2025-03-17 17:56:31 +0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,11 +8,11 @@ function Footer() {
       <span className="italic">心诚则灵</span>
       <a
         target="_blank"
-        href="https://github.com/sunls24/divination"
+        href="https://cloud.siliconflow.cn/i/VnptKAha"
         className="flex items-center gap-1 rounded p-1 transition-colors hover:bg-border"
       >
-        <Github size={16} />
-        <span className="underline underline-offset-2">v{VERSION}</span>
+{/*         <Github size={16} /> */}
+        <span className="underline underline-offset-2">硅基流动，支持众多AI大模型，注册送14元</span>
       </a>
     </footer>
   );
